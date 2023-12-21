e1b1c6f82b75f43b44eff5addb9846fa60975811
Phelipe Cores <83982430+PhelipeCores@users.noreply.github.com>
2023-12-21 15:50:44 +0100

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,23 @@
+
+
+
+//Scroling
+  let scrollContainer = document.querySelector('.gallery');
+  let backbtn = document.getElementById('backbtn');
+  let nextbtn = document.getElementById('nextbtn');
+
+  scrollContainer.addEventListener('wheel', (evt) => { 
+    evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  });
+  
+//Buttons
+  nextbtn.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = "smooth"
+    scrollContainer.scrollLeft += 900;
+  });
+  backbtn.addEventListener('click', () => {
+      scrollContainer.style.scrollBehavior = "smooth"
+    scrollContainer.scrollLeft -= 900;
+  });
+ 
